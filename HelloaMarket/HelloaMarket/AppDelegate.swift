b040292ddd3002f5f8e18bc68b2b10b791d82b66
glediaer <>
2022-03-11 00:08:15 +0900

    [IOS-HelloaMarket] 카카오 앱키 변경

--- HelloaMarket/HelloaMarket/AppDelegate.swift
+++ HelloaMarket/HelloaMarket/AppDelegate.swift
@@ -56,7 +56,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         application.registerForRemoteNotifications()
 
         // [END register_for_notifications]
-        KakaoSDKCommon.initSDK(appKey: "20a5a19074892fea22a99d21dc8cfb37")
+        KakaoSDKCommon.initSDK(appKey: "e133f4b7cdb3c63152a848744e606c60")
         
         return true
     }
