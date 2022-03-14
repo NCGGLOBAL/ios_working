5ad1828e9f425d6c9097683a3d04e35f9145d2a5
glediaer <>
2022-03-15 00:16:21 +0900

    [IOS-Metaceleb] 아이콘 홈 url 변경

--- Metaceleb/Metaceleb/AppDelegate.swift
+++ Metaceleb/Metaceleb/AppDelegate.swift
@@ -15,9 +15,9 @@ import KakaoSDKAuth
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     let gcmMessageIDKey = "gcm.message_id"
-    static var HOME_URL = "https://metaceleb.mallup.co.kr"
-    static let UPLOAD_URL = AppDelegate.HOME_URL + "/m/app/"
-    static let PUSH_REG_URL = AppDelegate.HOME_URL + "/m/app/pushRegister.asp"
+    static var HOME_URL = "https://metaceleb.com/default.asp"
+    static let UPLOAD_URL = "https://metaceleb.mallup.co.kr/m/app/"
+    static let PUSH_REG_URL = "https://metaceleb.mallup.co.kr/m/app/pushRegister.asp"
     static var LANDING_URL = ""
     static let deviceId = UIDevice.current.identifierForVendor?.uuidString
     static var QR_URL = ""
