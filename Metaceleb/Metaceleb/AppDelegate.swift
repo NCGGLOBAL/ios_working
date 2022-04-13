2f6564592cf6241fa94e5a6422d57e6ef8a51368
glediaer <>
2022-04-14 00:34:05 +0900

    [IOS-Hiniao] 프로젝트 생성

--- Metaceleb/Metaceleb/AppDelegate.swift
+++ Metaceleb/Metaceleb/AppDelegate.swift
@@ -56,7 +56,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         application.registerForRemoteNotifications()
 
         // [END register_for_notifications]
-        KakaoSDKCommon.initSDK(appKey: "20a5a19074892fea22a99d21dc8cfb37")
+        KakaoSDKCommon.initSDK(appKey: "fc5991fb8f95582fe48238fa974a4a69")
         
         return true
     }
