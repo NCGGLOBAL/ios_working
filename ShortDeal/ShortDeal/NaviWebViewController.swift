3ae6fbbe912ef5ce674beec2caeb3a4382594e72
glediaer <>
2022-08-18 00:00:15 +0900

    [IOS-ShortDeal] 네비게이션 웹뷰 작업

--- ShortDeal/ShortDeal/NaviWebViewController.swift
+++ ShortDeal/ShortDeal/NaviWebViewController.swift
@@ -490,6 +490,26 @@ class NaviWebViewController: UIViewController, WKUIDelegate, WKNavigationDelegat
             self.present(alertController, animated: true, completion: nil)
         }
     }
+    
+    func showToast(message : String) {
+            let width_variable:CGFloat = 10
+            let toastLabel = UILabel(frame: CGRect(x: width_variable, y: self.view.frame.size.height-150, width: view.frame.size.width-2*width_variable, height: 35))
+            // 뷰가 위치할 위치를 지정해준다. 여기서는 아래로부터 100만큼 떨어져있고, 너비는 양쪽에 10만큼 여백을 가지며, 높이는 35로
+            toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+            toastLabel.textColor = UIColor.white
+            toastLabel.textAlignment = .center;
+            toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+            toastLabel.text = message
+            toastLabel.alpha = 1.0
+            toastLabel.layer.cornerRadius = 10;
+            toastLabel.clipsToBounds  =  true
+            self.view.addSubview(toastLabel)
+            UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+                toastLabel.alpha = 0.0
+            }, completion: {(isCompleted) in
+                toastLabel.removeFromSuperview()
+            })
+        }
 
     @IBAction func onRefreshButton(_ sender: UIBarButtonItem) {
         self.initWebView()
@@ -497,6 +517,7 @@ class NaviWebViewController: UIViewController, WKUIDelegate, WKNavigationDelegat
     @IBAction func onCopyLinkButton(_ sender: UIBarButtonItem) {
         // 클립보드에 값 복사하기
         UIPasteboard.general.string = self.urlString
+        self.showToast(message: "링크가 복사되었습니다.")
     }
     @IBAction func onShareLinkButton(_ sender: UIBarButtonItem) {
         var shareItems = [String]()
