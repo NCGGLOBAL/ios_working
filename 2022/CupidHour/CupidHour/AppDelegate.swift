0eefa46567835f2ace912b4799cfaa83d98fa415
glediaer <>
2023-03-15 22:38:02 +0900

    [IOS-CupidHour] home url 변경

--- 2022/CupidHour/CupidHour/AppDelegate.swift
+++ 2022/CupidHour/CupidHour/AppDelegate.swift
@@ -13,7 +13,7 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     let gcmMessageIDKey = "gcm.message_id"
-    static var HOME_URL = "http://cupidhour.mallup.co.kr"
+    static var HOME_URL = "http://cupidhour.com"
     static let UPLOAD_URL = AppDelegate.HOME_URL + "/m/app/"
     static let PUSH_REG_URL = AppDelegate.HOME_URL + "/m/app/pushRegister.asp"
     static var LANDING_URL = ""
