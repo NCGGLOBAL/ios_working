303dcea71ef7c0fdcf03d23fffa31cdb8a157c88
glediaer <>
2023-09-25 22:55:46 +0900

    [IOS-InnerVersion] 인트로 이미지 2초 딜레이

--- 2023/InnerVersion/InnerVersion/AppDelegate.swift
+++ 2023/InnerVersion/InnerVersion/AppDelegate.swift
@@ -28,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Thread.sleep(forTimeInterval: 2.0)
         // Use Firebase library to configure APIs
         FirebaseApp.configure()
         // [START set_messaging_delegate]
