22a48dee72edaf2df26a5d9e5efc52e8adef3dd8
glediaer <>
2025-07-19 18:52:07 +0900

    [IOS-UnniTv] 현행화

--- 2025/UnniTv/UnniTv/AppDelegate.swift
+++ 2025/UnniTv/UnniTv/AppDelegate.swift
@@ -192,15 +192,14 @@ extension AppDelegate : MessagingDelegate {
     // [START refresh_token]
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
       print("Firebase registration token: \(String(describing: fcmToken))")
-
+        AppDelegate.pushkey = fcmToken ?? ""
       let dataDict: [String: String] = ["token": fcmToken ?? ""]
       NotificationCenter.default.post(
         name: Notification.Name("FCMToken"),
         object: nil,
         userInfo: dataDict
       )
-      // TODO: If necessary send token to application server.
-      // Note: This callback is fired at each app startup and whenever a new token is generated.
+        self.requestPushSetting()
     }
 
     // [END refresh_token]
